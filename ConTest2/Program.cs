64a8b1e2f47d9d9c5ee88d64c4531f57b92953c2
Fenky Tjhang <cornelis@hotmail.com>
2024-03-22 14:40:44 +0700

    Add project files.

---
+++ ConTest2/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.Configuration.EnvironmentVariables;
+using Microsoft.Extensions.Configuration.CommandLine;
+
+// See https://aka.ms/new-console-template for more information
+IConfigurationRoot configuration = new ConfigurationBuilder()
+    .AddEnvironmentVariables()
+    .AddJsonFile("./Config/config.json")
+    .AddCommandLine(args)
+    .Build();
+
+
+
+Console.WriteLine($"Hello, World! from second {configuration["name"]}");
+Console.ReadLine();
---
+++ consoletest/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.Configuration.EnvironmentVariables;
+using Microsoft.Extensions.Configuration.CommandLine;
+
+
+IConfigurationRoot configuration = new ConfigurationBuilder()
+    .AddEnvironmentVariables()
+    .AddJsonFile("./Config/config.json")
+    .AddCommandLine(args)
+    .Build();
+
+
+//var name = "test";
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine($"Hello from first, { configuration["name"] }");
+Console.ReadLine();
+
